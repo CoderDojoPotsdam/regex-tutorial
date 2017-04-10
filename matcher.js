8fc1af070037b3c72abb5d6008a603b03aab9bd1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-10 23:42:08 +0200

    bis wiederholungen

--- matcher.js
+++ matcher.js
@@ -1,9 +1,12 @@
 var errorMessages = {
-  "unterminated character class" : "Unverständlicher regulärer Ausdruck: eine Klammer wurde nicht geschlossen! Bitte schließe die Klammer wieder, damit der reguläre Ausdruck verarbeitet werden kann."
+  "unterminated character class" : "Unverständlicher regulärer Ausdruck: Eine Klammer wurde nicht geschlossen! Bitte schließe die Klammer wieder, damit der reguläre Ausdruck verarbeitet werden kann.  ",
+  "unterminated parenthetical": "Eine Klammer wurde nicht geschlossen. Bitte schreibe eine schließende Klammer \")\" in den regulären Ausdruck.",
+  "unmatched ) in regular expression": "Es gibt eine schließende Klammer aber dazu gibt es keine geöffnete Klammer. Schreibe eine geöffnete Klammer \"(\" vor die schließende Klammer \")\", um den Fehler zu beheben."
 }
 
 function translateErrorMessage(message) {
-  return errorMessages[message] || message;
+  var translation = (errorMessages[message] || "Bitte übersetzen.");
+  return "\"" + message + "\": " + translation;
 }
 
 function matchTextElement(string) {
