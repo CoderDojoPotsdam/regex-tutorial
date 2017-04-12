728b7dc4b7ebcb1601acc9791bff0871cc081598
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-12 14:23:58 +0200

    hints appear and disappear

---
+++ hint.js
@@ -0,0 +1,28 @@
+
+var hintClasses = ["clicked", "secret"];
+
+function watchHint(hint) {
+  var added = 0;
+  var wrap = document.createElement("div");
+  wrap.classList.add("hint-wrap");
+  hint.parentNode.replaceChild(wrap, hint);
+  var button = document.createElement("div");
+  button.classList.add("toggle-hint");
+  wrap.appendChild(button);
+  wrap.appendChild(hint);
+  function click() {
+    wrap.classList.remove(hintClasses[added]);
+    added = 1 - added;
+    wrap.classList.add(hintClasses[added]);
+  }
+  button.onclick = click;
+  click();
+}
+
+window.addEventListener("load", function(){
+  var hints = document.getElementsByClassName("hint");
+  for (var i = 0; i < hints.length; i += 1) {
+    var hint = hints[i];
+    watchHint(hint);
+  }
+});
