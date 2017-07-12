236afddcccfa95b48e3418bd8b688c4b8077f84f
Aron Bergman <bathingrad@gmail.com>
2017-07-12 18:43:30 +0200

    Translated js file

---
+++ js/lang-sv.js
@@ -0,0 +1,14 @@
+var errorMessages = {
+  "oavslutad teckenklass" : "Obearbetbart reguljärt uttryck: En hakparentes blev inte stängd! Stäng hakparentesen så att det reguljära uttrycket kan användas.",
+  "oavslutad parentes": "En parentes blev inte stängd. Lägg till en parentes till det reguljära uttrycket.",
+  "omatchad ) i det reguljära uttrycket": "Det finns en stängande parentes men ingen öppnande parentes. Lägg till en \"(\" till det reguljära uttrycket för att ta bort det här felmeddelandet.",
+  "ingenting att upprepa": "Finns inget att upprepa. \"+\", \"*\" eller andra repetitioner refererar inte till något som går att upprepa."
+}
+var errorReferenceWrong = "<br/>Den här hemsidan har progammeringsfel. Det är ett fel i källkoden: referensmplementeringen av det reguljära uttrycket kan inte användas. Vänligen klick på \"förbättra sidan\" nedan eller <a href='https://github.com/CoderDojoPotsdam/regex-tutorial/issues/new'>skapa en felrapport</a>.";
+
+var ERROR_MESSAGE = "Felmeddelande";
+
+var PLEASE_TRANSLATE = "Vänligen översätt.";
+
+var YES = "Ja";
+var NO = "Nej";
